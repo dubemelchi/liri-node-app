c785945002a696b4ce23d3ce5139213870f7dfd8
dubemelchi <dubemelchi@gmail.com>
2019-07-20 04:40:29 -0400

    console.log for api keys

--- liri.js
+++ liri.js
@@ -2,11 +2,16 @@
 // dotenv is the module that will load the api id and secret from the .env file.
 require("dotenv").config();
 
+
+
 // variable importing the node-spotify-api npm package.
 var mySpotify = require("node-spotify-api");
 
 // variable importing the api key.
 var myKeys = require("./keys");
+
+// check api keys imported correctly.
+console.log(myKeys);
 
 // variable importing the axios npm package.
 var axios = require("axios");
@@ -19,6 +24,8 @@ var fs = require("fs");
 
 // var initializing the spotify api.
 var spotify = new mySpotify(keys.spotify);
+
+
 
 
 
