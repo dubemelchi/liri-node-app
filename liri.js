631ba9f970f22cf4535033dde75be83f59625726
dubemelchi <dubemelchi@gmail.com>
2019-07-20 04:35:04 -0400

    declared variables for application.

---
+++ liri.js
@@ -0,0 +1,24 @@
+
+// dotenv is the module that will load the api id and secret from the .env file.
+require("dotenv").config();
+
+// variable importing the node-spotify-api npm package.
+var mySpotify = require("node-spotify-api");
+
+// variable importing the api key.
+var myKeys = require("./keys");
+
+// variable importing the axios npm package.
+var axios = require("axios");
+
+// var importing the moment npm.
+var moment = require("moment");
+
+// variable importing the fs package for read/write.
+var fs = require("fs");
+
+// var initializing the spotify api.
+var spotify = new mySpotify(keys.spotify);
+
+
+
